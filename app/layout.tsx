440604d75bda2dd4367659e6ab73ba02b38d430d
Gaurab <dhakalgaurab123@gmail.com>
2025-02-05 10:48:01 +0545

    small tweak

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
             <body
                 className={`${interSans.className} antialiased min-h-screen flex flex-col bg-black bg-[radial-gradient(#262d26_1px,transparent_1px)] [background-size:16px_16px] selection:bg-[#e0e0e0] selection:text-[#3f3f3f]`}
             >
-                <div className="fixed bottom-0 lg:fixed lg:top-0 lg:bottom-auto w-full z-50 backdrop-blur-md">
+                <div className="fixed bottom-0 lg:fixed lg:top-0 lg:bottom-auto w-full z-50 sm:backdrop-blur-md">
                     <Navbar />
                 </div>
                 <main className="flex-grow p-4 lg:p-0 mb-16 lg:mb-0 lg:mt-16">
