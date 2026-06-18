b67888dae431a99d51e700488d5befb9a1e92130
Gaurab Dhakal <dhakalgaurab123@gmail.com>
2026-06-18 19:36:13 +0545

    Refactor code structure for improved readability and maintainability

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,14 +2,20 @@
 
 import { FULL_NAME, BASIC_EXPANDED_INTRO_TEXT } from "@/constants/bio";
 import { ButtonWrapper } from "./ButtonWrapper";
+import localFont from "next/font/local";
+
+const PP_Neue_Montreal = localFont({
+  src: "../public/fonts/PPNeueMontreal/PPNeueMontreal-Extrabold.otf",
+});
 
 const Intro = () => {
   return (
     <div className="w-full flex flex-col gap-5">
       <div className="space-y-5">
         <div className="md:text-6xl flex text-4xl font-extrabold  ">
-          <p className="text-gray-400">
-            Hi, I am <br className="md:hidden" /> <span className="text-white">{FULL_NAME}</span>.
+          <p className="text-gray-400 ">
+            Hi, I am <br className="md:hidden" />{" "}
+            <span className={`${PP_Neue_Montreal.className} text-white `}>{FULL_NAME}</span>.
           </p>
         </div>
         <div className="md:max-w-210 text-neutral-300/75 font-medium md:px-0.5">
